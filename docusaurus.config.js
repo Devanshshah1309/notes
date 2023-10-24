b09561b65305162e4101650fb5d9d3da21433da9
DevanshShah1309 <devanshshah1309@gmail.com>
2023-10-24 22:26:39 +0800

    Add logo and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
         title: "Knowledge Repository",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo.png",
         },
         items: [
           {
