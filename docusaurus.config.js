91b0665ed997bf00af4cdfb53179af0cd7c1753d
Devansh Shah <devanshshah1309@gmail.com>
2024-01-29 16:07:21 +0800

    Update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/logo.png",
       navbar: {
         title: "Knowledge Repository",
         logo: {
